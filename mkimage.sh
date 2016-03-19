9fabe4616ae082105a0fcc1643df344b11fe547a
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2016-03-19 17:06:05 +0100

    .gitignore
    rebase image build on CC15.05.1

--- mkimage.sh
+++ mkimage.sh
@@ -7,20 +7,20 @@ FILES="$PWD/files"
 
 
 # download image builder for the ar71xx platform
-if [ -s OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64.tar.bz2 ]; then
+if [ -s OpenWrt-ImageBuilder-15.05.1-ar71xx-generic.Linux-x86_64.tar.bz2 ]; then
     echo "image builder already downloaded do not download it again"
 else
-    wget http://downloads.openwrt.org/chaos_calmer/15.05/ar71xx/generic/OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64.tar.bz2
+    wget https://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/generic/OpenWrt-ImageBuilder-15.05.1-ar71xx-generic.Linux-x86_64.tar.bz2
     echo "image builder downloaded"
 fi
 
-if [ -s OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64 ]; then
+if [ -s OpenWrt-ImageBuilder-15.05.1-ar71xx-generic.Linux-x86_64 ]; then
     echo "image builder already unpacked do not unpack it again"
 else
-    tar xjf OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64.tar.bz2
+    tar xjf OpenWrt-ImageBuilder-15.05.1-ar71xx-generic.Linux-x86_64.tar.bz2
     echo "image builder unpacked"
 fi
 
-cd OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64
+cd OpenWrt-ImageBuilder-15.05.1-ar71xx-generic.Linux-x86_64
 make clean
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES"
